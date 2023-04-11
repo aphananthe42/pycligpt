6109dd290f546a0df69fa1f1b031156a30d60d23
Aphananthe <aphananthe42@gmail.com>
2023-04-11 23:53:49 +0900

    Fix: main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+from dotenv import load_dotenv
+
+import chat_gpt
+
+
 def main():
-    print("atcoder-template")
+    load_dotenv()
+    gpt = chat_gpt.ChatGPT(timeout=60)
+    gpt.request_to_gpt(prompt="こんにちは")
 
 
 if __name__ == "__main__":
