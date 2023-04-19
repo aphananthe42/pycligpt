e1c952927b0ca3c8b2db1848139f3d1ccb3bc4eb
Aphananthe <aphananthe42@gmail.com>
2023-04-20 00:24:50 +0900

    Add: Arguparser

--- src/main.py
+++ src/main.py
@@ -1,13 +1,29 @@
+import argparse
+
 from dotenv import load_dotenv
 
 import chat_gpt
 
 
-def main():
+def main(args: argparse.Namespace):
     load_dotenv()
-    gpt = chat_gpt.ChatGPT(timeout=60)
+
+    timeout = args.timeout
+    gpt = chat_gpt.ChatGPT(timeout)
+
     gpt.request_to_gpt(prompt="こんにちは")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Config argument to set up ChatGPT")
+    parser.add_argument("--model", action="store_true", help="Use GPT-4 or not")
+    parser.add_argument(
+        "-t", "--timeout", type=int, help="Timeout until receive response"
+    )
+    parser.add_argument(
+        "-m", "--multi", action="store_true", help="Enable multi-line mode"
+    )
+    parser.add_argument("-r", "--raw", action="store_true", help="Enable raw mode")
+    args = parser.parse_args()
+
+    main(args)
