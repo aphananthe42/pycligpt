606fe741b4bdab0722379579eeedfe8af5cbb9c5
Aphananthe <aphananthe42@gmail.com>
2023-06-11 18:33:11 +0900

    Fix: defalut value of environment value

--- pycligpt/settings.py
+++ pycligpt/settings.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-REQUEST_TIMEOUT = os.environ["REQUEST_TIMEOUT"]
-GPT_MODEL = os.environ["GPT_MODEL"]
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+GPT_MODEL = os.environ.get("GPT_MODEL", "gpt-3.5-turbo")
+REQUEST_TIMEOUT = os.environ.get("REQUEST_TIMEOUT", 30)
